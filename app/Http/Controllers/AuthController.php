c39374c352f0d8e19b07e2cbe2ac7b8f02e884a4
Denniel Joshua Diaz <denniel.joshua@gmail.com>
2016-12-01 15:50:18 +0800

    install fb sdk

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\JWTAuth;
+use Facebook\Facebook;
+
 use Auth;
 
 class AuthController extends Controller
@@ -62,6 +64,24 @@ class AuthController extends Controller
 
     public function fblogin(Request $request)
     {
-        return response()->json($request);
+        $fb = new Facebook([
+            'app_id' => '273246269726674',
+            'app_secret' => '4d37b7b20c749a30c90f9d709c2c4a55',
+            'default_graph_version' => 'v2.7',
+        ]);
+
+        try {
+            $response = $fb->get('/me?fields=id,name', $request['authResponse']['accessToken']);
+        }  catch(Facebook\Exceptions\FacebookResponseException $e) {
+          echo 'Graph returned an error: ' . $e->getMessage();
+          exit;
+        } catch(Facebook\Exceptions\FacebookSDKException $e) {
+          echo 'Facebook SDK returned an error: ' . $e->getMessage();
+          exit;
+        }
+
+        $user = $response->getGraphUser();
+
+        return response()->json($user->getProperty('first_name'));
     }
 }
