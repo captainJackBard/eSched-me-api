4c396c7298fc6be8acf3d94ff69adc760d4080f2
captainJackBard <fcktardjoe@gmail.com>
2016-12-24 14:42:22 +0800

    add location

---
+++ app/Transformers/LocationTransformer.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Transformers;
+
+use App\Location;
+use League\Fractal;
+
+class LocationTransformer extends Fractal\TransformerAbstract
+{
+    /**
+     * List of resources possible to include
+     *
+     * @var array
+     */
+    protected $availableIncludes = [
+        'activity',
+    ];
+
+
+    public function transform(Location $meeting)
+    {
+        return [
+            'id' => $meeting->id,
+            'activty_id' => $meeting->activity_id,
+            'project_name' => $meeting->activity->title,
+            'location' => $meeting->location,
+            'long' => $meeting->long,
+            'lat' => $meeting->lat,
+            'status' => $meeting->status,
+            'date' => $meeting->date,
+        ];
+    }
+
+    public function includeActivity(Location $location)
+    {
+        $activity = $location->activity;
+        return $this->collection($activity, new ActivityTransformer());
+    }
+
+}
