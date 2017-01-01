5bd6e312fe4cfcee891e55e888e75493fbff47d8
captainJackBard <fcktardjoe@gmail.com>
2017-01-01 22:17:51 +0800

    fix not capital thingy

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,13 +31,13 @@ class Kernel extends ConsoleKernel
         $schedule->call(function() {
             // TODO : This cron job will run everyminute, check if the project is 
             // and change its status to 'failed'
-            $projects = Activity::where('end', '<=', new DateTime())->where('status', 'Ongoing');
-            $projects->update(["status" => "Failed"]);
+            $projects = Activity::where('end', '<=', new DateTime())->where('status', 'ongoing');
+            $projects->update(["status" => "failed"]);
             // TODO : Optionally broadcast an event here that a project has failed once we
             // implemented the laravel events and socket.io or pusher
 
-            $modules = Module::where('end', '<=', new DateTime())->where('status', 'Ongoing');
-            $modules->update(["status" => "Failed"]);
+            $modules = Module::where('end', '<=', new DateTime())->where('status', 'ongoing');
+            $modules->update(["status" => "failed"]);
         });
     }
 }
